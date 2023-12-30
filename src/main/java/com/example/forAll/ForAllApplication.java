cc374ac27bcaa306bc00295622a789c8e7f8652f
0921sean <0921sean@gmail.com>
2023-12-30 16:17:17 +0900

    start

---
+++ src/main/java/com/example/forAll/ForAllApplication.java
@@ -0,0 +1,13 @@
+package com.example.forAll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForAllApplication.class, args);
+	}
+
+}
