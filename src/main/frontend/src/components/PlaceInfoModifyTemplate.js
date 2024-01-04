7fb05ff980eba86345c19b3790445571a3dc9ab4
kimyt0511 <kimyt0511@yonsei.ac.kr>
2024-01-04 21:59:35 +0900

    [01.04 윤태] 개인정보수정 테스트 완료

--- src/main/frontend/src/components/PlaceInfoModifyTemplate.js
+++ src/main/frontend/src/components/PlaceInfoModifyTemplate.js
@@ -1,23 +1,32 @@
 import { useState, useCallback } from "react";
 import {Link} from "react-router-dom";
+import axios from "axios";
 
 const PlaceTemplate = () => {
-    const [id, setId] = useState('');
+    const id = sessionStorage.getItem("user_id");
     const [passwd, setPasswd] = useState('');
 
-    const onChangeId = useCallback((e) => {
-        setId(e.target.value);
-    },[]);
     const onChangePw = useCallback((e) => {
         setPasswd(e.target.value);
     },[]);
+
+    const checkPw = () => {
+        if (passwd === "") alert("비밀번호를 입력해주세요");
+        else{
+            axios.get("/api/v1/members/"+id+"/"+passwd)
+                .then(() => {
+                    window.location.href = "/placeInfoModify2";
+                }).catch(()=>{
+                    alert("비밀번호가 일치하지 않습니다");
+            });
+        }
+    }
     return (
         <div>
             <div>
                 <input
-                    placeholder="아이디 입력"
                     value={id}
-                    onChange={onChangeId}
+                    readOnly={true}
                 />
                 <input
                     placeholder="비밀번호 입력"
@@ -26,9 +35,7 @@ const PlaceTemplate = () => {
                 />
             </div>
             <div>
-                <Link to="/placeInfoModify2">
-                    <button className="button">확인</button>
-                </Link>
+                    <button className="button" onClick={() => checkPw()}>확인</button>
 
             </div>
         </div>
--- src/main/frontend/src/pages/PersonalInfoModifyPage2.js
+++ src/main/frontend/src/pages/PersonalInfoModifyPage2.js
@@ -9,19 +9,20 @@ import "../components/Styles.css";
 const PersonalModify2 = () => {
     const location = useLocation();
     const data = { ...location.state };
+    const birdDayList = data.birthDay != null ? data.birthDay.split('/') : "//".split("/");
     const navigate = useNavigate();
-    const [id, setId] = useState('');
+    const [id, setId] = useState(data.loginId);
     const [pw, setPw] = useState('');
     const [pwCheck, setPwCheck] = useState('');
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
+    const [name, setName] = useState(data.name);
+    const [email, setEmail] = useState(data.email);
+    const [phone, setPhone] = useState(data.phoneNum);
     const [cerifiedNum, setCerifiedNum] = useState('');
-    const [birthDay, setBirthDay] = useState('');
-    const [year, setYear] = useState("");
-    const [month, setMonth] = useState("");
-    const [day, setDay] = useState("");
-    const [gender, setGender] = useState("");
+    const [birthDay, setBirthDay] = useState(data.birthDay);
+    const [year, setYear] = useState(birdDayList[0]);
+    const [month, setMonth] = useState(birdDayList[1]);
+    const [day, setDay] = useState(birdDayList[2]);
+    const [gender, setGender] = useState(data.gender);
 
     const [isCheckDuplicatedId, setIsCheckDuplicatedId] = useState();
     const [isCheckDuplicatedEmail, setIsCheckDuplicatedEmail] = useState();
@@ -52,33 +53,30 @@ const PersonalModify2 = () => {
     useEffect(() => {
         setBirthDay(year + '/' + month + '/' + day);
     }, [year, month, day]);
-
-    useEffect(() => {
-        console.log(gender);
-    }, [gender]);
     const handleButton = () => {
         //Todo : 아이디, 이메일 중복 여부 확인, 비밀번호 확인 여부, 휴대폰 인증 여부, 약관 동의 여부 등 확인
         if (id === "") {
             alert("아이디는 필수 입력 사항입니다");
-        } else if (pw === "") {
-            alert("비밀번호는 필수 입력 사항입니다");
-        } else if (name === "") {
-            alert("이름은 필수 입력 사항입니다");
-        } else if (email === "") {
-            alert("이메일은 필수 입력 사항입니다");
-        } else if (phone === "") {
-            alert("휴대폰 번호는 필수 입력 사항입니다");
-        } else if ((year === "") || (month === "") || (day === "")) {
-            alert("생년월일은 필수 입력 사항입니다");
         }
-        else if (isCheckDuplicatedId !== true) {
-            alert("아이디 중복확인이 필요합니다");
-        } else if (isCheckPw !== true) {
-            alert("비밀번호가 일치하지 않습니다");
-        }
-        else if (isCheckDuplicatedEmail !== true) {
-            alert("이메일 중복확인이 필요합니다");
-        }
+        // else if (pw === "") {
+        //     alert("비밀번호는 필수 입력 사항입니다");
+        // } else if (name === "") {
+        //     alert("이름은 필수 입력 사항입니다");
+        // } else if (email === "") {
+        //     alert("이메일은 필수 입력 사항입니다");
+        // } else if (phone === "") {
+        //     alert("휴대폰 번호는 필수 입력 사항입니다");
+        // } else if ((year === "") || (month === "") || (day === "")) {
+        //     alert("생년월일은 필수 입력 사항입니다");
+        // }
+        // else if (isCheckDuplicatedId !== true) {
+        //     alert("아이디 중복확인이 필요합니다");
+        // } else if (isCheckPw !== true) {
+        //     alert("비밀번호가 일치하지 않습니다");
+        // }
+        // else if (isCheckDuplicatedEmail !== true) {
+        //     alert("이메일 중복확인이 필요합니다");
+        // }
         else {
             axios.put("/api/v1/members", //이 부분이 불명확해서 수정이 필요할 수 있음
                 {
@@ -117,8 +115,14 @@ const PersonalModify2 = () => {
         <div>
             <Header PageName={"개인정보수정"} />
             <PersonalInfoInputTemplate
-                pw={pw}
-                role={data.role}
+                id={id}
+                name={name}
+                phone={phone}
+                email={email}
+                year={year}
+                month={month}
+                day={day}
+                gender={gender}
                 setId={setId}
                 setPw={setPw}
                 setPwCheck={setPwCheck}
@@ -135,7 +139,6 @@ const PersonalModify2 = () => {
                 isCheckedDuplicatedId={isCheckDuplicatedId}
                 isCheckedDuplicatedEmail={isCheckDuplicatedEmail}
                 isCheckPw={isCheckPw}
-                gender={gender}
             />
             <Link to="/personalInfoModify">
                 <button>뒤로</button>
