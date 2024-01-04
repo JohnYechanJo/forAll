c8f58432b1c1af557af08e249b89404c3173c291
0921sean <0921sean@gmail.com>
2024-01-04 21:22:18 +0900

    [01.04 승범]

--- src/main/java/project/forAll/controller/api/APILoginController.java
+++ src/main/java/project/forAll/controller/api/APILoginController.java
@@ -1,20 +1,15 @@
 package project.forAll.controller.api;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import project.forAll.domain.Member;
 import project.forAll.form.LoginForm;
 import project.forAll.service.MemberService;
 import project.forAll.web.SessionConst;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @RestController
@@ -33,11 +28,6 @@ public class APILoginController extends APIController {
 
             HttpSession session = request.getSession();
             session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
-
-//            // 쿠키 생성
-//            Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
-//            response.addCookie(idCookie);
-
             return new ResponseEntity(loginMember, HttpStatus.OK);
         }catch(final Exception e){
             return new ResponseEntity(errorResponse("Could not find member : " + e.getMessage()), HttpStatus.BAD_REQUEST);
@@ -51,14 +41,5 @@ public class APILoginController extends APIController {
         if (session != null) {
             session.invalidate();
         }
-//        // 쿠키 만료
-//        expireCookie(response, "memberId");
     }
-
-//    private void expireCookie(HttpServletResponse response, String cookieName) {
-//        Cookie cookie=new Cookie(cookieName,null);
-//        cookie.setMaxAge(0);
-//        response.addCookie(cookie);
-//    }
-
 }
