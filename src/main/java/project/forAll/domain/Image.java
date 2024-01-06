68e414739a77e277fcef9008928bc38b934a10e8
0921sean <0921sean@gmail.com>
2024-01-07 03:34:27 +0900

    [01.07 승범] 도메인 파일위치 정리, 공간정보 도메인 추가

--- src/main/java/project/forAll/domain/Image.java
+++ src/main/java/project/forAll/domain/Image.java
@@ -16,6 +16,8 @@ public class Image extends BassDomain {
     @Column(name = "image_id")
     private Long id;
 
+    // 원래 이미지 이름
     private String originName;
+    // 같은 이름을 가진 이미지의 충돌을 막기 위한 random uuid 저장 변수
     private String imageName;
 }
