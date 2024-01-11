5e1b9b6579547ed02fb143a6df5bb18b8cf0b606
0921sean <0921sean@gmail.com>
2024-01-11 19:43:23 +0900

    [01.11 승범] 카카오로그인 구현 완료

--- src/main/frontend/src/pages/LoginHandeler.js
+++ src/main/frontend/src/pages/LoginHandeler.js
@@ -11,15 +11,15 @@ const LoginHandeler = (props) => {
         const kakaoLogin = async () => {
             await axios({
                 method: "GET",
-                url: `/api/v1/login/oauth2/callback/kakao?code=${code}`,
+                url: `http://localhost:3000/api/v1/login/oauth2/callback/kakao?code=${code}`,
                 headers: {
                     "Content-Type": "application/json;charset=utf-8", //json형태로 데이터를 보내겠다는뜻
-                    // "Access-Control-Allow-Origin": "*", //이건 cors 에러때문에 넣어둔것. 당신의 프로젝트에 맞게 지워도됨
+                    "Access-Control-Allow-Origin": "*", //이건 cors 에러때문에 넣어둔것. 당신의 프로젝트에 맞게 지워도됨
                 },
             }).then((res) => { //백에서 완료후 우리사이트 전용 토큰 넘겨주는게 성공했다면
                 console.log(res);
                 //계속 쓸 정보들( ex: 이름) 등은 localStorage에 저장해두자
-                sessionStorage.setItem("name", res.data.account.kakaoName);
+                sessionStorage.setItem("name", res.data.kakaoMember.id);
                 //로그인이 성공하면 이동할 페이지
                 navigate("/main");
             }).catch((err) => console.error(err));
