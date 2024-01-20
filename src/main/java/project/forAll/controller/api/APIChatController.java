a37f237870a1b18758fa0fb948f68644bd0e4583
kimyt0511 <kimyt0511@yonsei.ac.kr>
2024-01-20 21:00:59 +0900

    [01.20 윤태] 채팅 프런트 백 연결
    - 사진 첨부기능 추가 필요

--- src/main/java/project/forAll/controller/api/APIChatController.java
+++ src/main/java/project/forAll/controller/api/APIChatController.java
@@ -19,6 +19,7 @@ import project.forAll.form.ChatRoomForm;
 import project.forAll.form.MessageForm;
 import project.forAll.service.Chat.ChatRoomService;
 import project.forAll.service.Chat.MessageService;
+import retrofit2.http.Path;
 
 import java.util.List;
 
@@ -30,14 +31,15 @@ public class APIChatController extends APIController{
     private final SimpMessagingTemplate messagingTemplate;
 
     /**
-     * 유저 아이디에 해당하는 채팅창 정보들을 받아옴
-     * @param userId 유저 아이디
-     * @return 채팅창 리스트
+     * 사용자 아이디와 카테고리에 해당하는 채팅방 list 반환
+     * @param userId
+     * @param category
+     * @return chatroomlist
      */
-    @GetMapping("/chat/roomlist/{id}")
-    public ResponseEntity getUserRoomList(@PathVariable(value = "id") final String userId){
+    @GetMapping("/chat/roomlist/{id}/{category}")
+    public ResponseEntity getUserRoomList(@PathVariable(value = "id") final String userId, @PathVariable String category){
         try{
-            List<ChatRoom> chatRoomList = chatRoomService.getRoomList(userId);
+            List<ChatRoom> chatRoomList = chatRoomService.getCategorizedChatRoom(userId, category);
             List<ChatRoomForm> chatRoomForms = chatRoomList.stream().map(chatRoom -> chatRoomService.of(chatRoom)).toList();
 
             return new ResponseEntity(chatRoomForms, HttpStatus.OK);
@@ -51,7 +53,7 @@ public class APIChatController extends APIController{
      * @param id 채팅창 id
      * @return message list
      */
-    @GetMapping("/chat/{id}")
+    @GetMapping("/chat/message/{id}")
     public ResponseEntity getChatRoomMessage(@PathVariable Long id){
         try{
             final ChatRoom chatRoom = (ChatRoom) chatRoomService.findById(id);
@@ -67,11 +69,11 @@ public class APIChatController extends APIController{
      * @param userId2
      * @return 채팅방 id
      */
-    @GetMapping("/chat/join/{userId1}/{userId2}")
-    public ResponseEntity joinChatRoom(@PathVariable String userId1, @PathVariable String userId2){
+    @GetMapping("/chat/join/{userId1}/{userId2}/{category}")
+    public ResponseEntity joinChatRoom(@PathVariable String userId1, @PathVariable String userId2, @PathVariable String category){
         try{
-            final ChatRoom chatRoom = chatRoomService.getChatRoom(userId1, userId2);
-            return new ResponseEntity(Long.toString(chatRoom.getId()), HttpStatus.OK);
+            final ChatRoom chatRoom = chatRoomService.getChatRoom(userId1, userId2, category);
+            return new ResponseEntity(chatRoomService.of(chatRoom), HttpStatus.OK);
         }catch (final Exception e){
             return new ResponseEntity(errorResponse("Could not join Chat Room : " + e.getMessage()), HttpStatus.BAD_REQUEST);
         }
