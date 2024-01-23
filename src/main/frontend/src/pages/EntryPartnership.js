2e3321430a16f4e1fd6f5aa0ed4da5b5ef46a756
johnjo <johnjo1116@gmail.com>
2024-01-23 19:40:31 +0900

    [01.23 예찬] 홈화면에 자주 묻는 질문, 입점 및 제휴 문의 프론트 추가

--- src/main/frontend/src/pages/EntryPartnership.js
+++ src/main/frontend/src/pages/EntryPartnership.js
@@ -1,27 +1,52 @@
-import {useCallback, useEffect, useState} from "react";
-import {BoardCategory, MainPageType} from "../../utils/enums";
+
+import Sidebar from "../../src/components/home/Sidebar";
+import HomeTemplate from "../../src/components/home/HomeTemplate";
+import {useLocation, useNavigate} from "react-router-dom";
+import {useEffect, useRef, useState} from "react";
 import axios from "axios";
-import Sidebar from "../../components/home/Sidebar";
-import HomeTemplate from "../../components/home/HomeTemplate";
-import ArticleListTemplate from "../../components/board/ArticleListTemplate";
-import {TimeUtil} from "../../utils/TimeUtil";
-import ImageUploader from "../../utils/imageUploader";
-import {useNavigate} from "react-router-dom";
-import ImageViewer from "../../components/ImageViewer";
-import ImagePreView from "../../components/ImagePreView";
+import {BoardCategory} from "../utils/enums";
+
 
 const EntryPartnership = () => {
     const navigate = useNavigate();
-    const [postList, setPostList] = useState([]);
-    const [newPost, setNewPost] = useState(false);
-    const [postTitle, setPostTitle] = useState("");
-    const [postContent, setPostContent] = useState("");
-    const [postImage, setPostImage] = useState([]);
-
-    const postContentPlaceholder = "포 올은 누구나 기분 좋게 참여할 수 있는 커뮤니티를 만들기 위해 커뮤니티 이용규칙을 제정하여 운영하고 있습니다. 위반 시 게시물이 삭제되고 서비스 이용이 일정 기간 제한될 수 있습니다.\n\n 아래는 이 게시판에 해당하는 핵심 내용에 대한 요약 사항이며, 게시물 작성 전 커뮤니티 이용규칙 전문을 반드시 확인하시기 바랍니다.\n\n※ 정치, 사회 관련 행위 금지\n-국가기관, 정치 관련 단체, 언론, 시민단체에 대한 언급 혹인 이와 관련한 행위\n-정책, 외교 또는 정책, 정파에 대한 의견, 주장 및 이념, 가치관을 드러내는 행위\n-성별, 종교, 인종, 출신, 지역, 직업, 이념 등 사회적 이유에 대한 언급 혹은 이와 관련한 행위\n,-위와 같은 내용으로 유추될 수 있는 비유, 은어 사용 행위\n*해당 게시물은 시사, 이슈 게시판에만 작성 가능합니다.";
-    const onChangePostTitle = useCallback((e)=>setPostTitle(e.target.value),[]);
-    const onChangePostContent = useCallback((e)=>setPostContent(e.target.value),[]);
-
+    const location = useLocation();
+    const data = {...location.state};
+    const [spaceData, setSpaceData] = useState([]);
+    const [popupData, setPopupData] = useState([]);
+    const [chatData, setChatData] = useState([]);
+    const [recipeData, setRecipeData] = useState([]);
+    const spaceRef = useRef();
+    const boardRef = useRef();
+    const mainPageRef = useRef();
+    const spaceFocus = () => { spaceRef.current?.scrollIntoView({ behavior: 'smooth' })};
+    const boardFocus = () => { boardRef.current?.scrollIntoView({ behavior: 'smooth' })};
+    useEffect(() => {
+        axios.get("/api/v1/space/isPublic")
+            .then((res) => setSpaceData(res.data))
+            .catch((err) => console.error(err));
+        axios.get("/api/v1/articles/category/" + BoardCategory.Popup)
+            .then((res) => setPopupData(res.data.sort((a,b) => {
+                if(a.writtenAt > b.writtenAt) return -1;
+                else return 1;
+            }).slice(0,3)))
+            .catch((err) => console.error(err));
+        axios.get("/api/v1/articles/category/" + BoardCategory.Chat)
+            .then((res) => setChatData(res.data.sort((a,b) => {
+                if(a.writtenAt > b.writtenAt) return -1;
+                else return 1;
+            }).slice(0,3)))
+            .catch((err) => console.error(err));
+        axios.get("/api/v1/articles/category/" + BoardCategory.Recipe)
+            .then((res) => setRecipeData(res.data.sort((a,b) => {
+                if(a.writtenAt > b.writtenAt) return -1;
+                else return 1;
+            }).slice(0,3)))
+            .catch((err) => console.error(err));
+    }, []);
+    useEffect(() => {
+        if (data.focus === "space") spaceFocus();
+        else if (data.focus === "board") boardFocus();
+    }, [mainPageRef]); // 페이지 전환 후 스크롤 이동에 애먹고 있음
     return(
         <div>
             <div className="header" style={{backgroundColor:"white"}}> {/*헤더에 뒤로가기 버튼 집어넣기*/}
