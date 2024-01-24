a256f204026dcddca12285ad9392aed17bc0c9c1
johnjo <johnjo1116@gmail.com>
2024-01-25 03:26:04 +0900

    pull

--- src/main/java/project/forAll/repository/space/PlaceRepository.java
+++
@@ -1,9 +0,0 @@
-package project.forAll.repository.space;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import project.forAll.domain.space.Place;
-
-@Repository
-public interface PlaceRepository extends JpaRepository<Place, Long> {
-}
--- src/main/java/project/forAll/repository/space/RentRepository.java
+++
@@ -1,9 +0,0 @@
-package project.forAll.repository.space;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import project.forAll.domain.space.Rent;
-
-@Repository
-public interface RentRepository extends JpaRepository<Rent, Long> {
-}
