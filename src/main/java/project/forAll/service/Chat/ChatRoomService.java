9fd5c5ea6abd2e5f4d9796d60d2e37db32a090ec
kimyt0511 <kimyt0511@yonsei.ac.kr>
2024-01-24 20:10:55 +0900

    [01.24 윤태] 카테고리가 다를 때, 중복된 채팅방 생성 오류 수정

--- src/main/java/project/forAll/service/Chat/ChatRoomService.java
+++ src/main/java/project/forAll/service/Chat/ChatRoomService.java
@@ -32,7 +32,7 @@ public class ChatRoomService extends Service {
     }
 
     public ChatRoom getChatRoom(String userId1, String userId2, String category){
-        List<ChatRoom> chatRoom = chatRoomRepository.findByUserLoginIds(userId1, userId2);
+        List<ChatRoom> chatRoom = chatRoomRepository.findByUserLoginIdsAndCategory(userId1, userId2, ChatRoomCategory.parse(category));
         if (chatRoom.isEmpty()){
             final ChatRoom newRoom = new ChatRoom();
             List<Member> users = new ArrayList<>();
