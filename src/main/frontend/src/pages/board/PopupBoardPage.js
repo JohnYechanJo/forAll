a4e15630a5ea6c73d296e977ae3219de3ebb4fca
kimyt0511 <kimyt0511@yonsei.ac.kr>
2024-01-26 05:04:13 +0900

    [01.26 윤태] 새 글 작성 시 양식 디자인

--- src/main/frontend/src/pages/board/PopupBoardPage.js
+++ src/main/frontend/src/pages/board/PopupBoardPage.js
@@ -11,6 +11,7 @@ import ImageViewer from "../../components/ImageViewer";
 import ImagePreView from "../../components/ImagePreView";
 import updownImg from "../../components/icons/updown.jpg";
 import pencilImg from "../../components/icons/pencil.jpg";
+import clip from "../../components/icons/clip.png";
 
 const PopupBoardPage = () => {
     const navigate = useNavigate();
@@ -20,7 +21,7 @@ const PopupBoardPage = () => {
     const [postContent, setPostContent] = useState("");
     const [postImage, setPostImage] = useState([]);
 
-    const postContentPlaceholder = "포 올은 누구나 기분 좋게 참여할 수 있는 커뮤니티를 만들기 위해 커뮤니티 이용규칙을 제정하여 운영하고 있습니다. 위반 시 게시물이 삭제되고 서비스 이용이 일정 기간 제한될 수 있습니다.\n\n 아래는 이 게시판에 해당하는 핵심 내용에 대한 요약 사항이며, 게시물 작성 전 커뮤니티 이용규칙 전문을 반드시 확인하시기 바랍니다.\n\n※ 정치, 사회 관련 행위 금지\n-국가기관, 정치 관련 단체, 언론, 시민단체에 대한 언급 혹인 이와 관련한 행위\n-정책, 외교 또는 정책, 정파에 대한 의견, 주장 및 이념, 가치관을 드러내는 행위\n-성별, 종교, 인종, 출신, 지역, 직업, 이념 등 사회적 이유에 대한 언급 혹은 이와 관련한 행위\n,-위와 같은 내용으로 유추될 수 있는 비유, 은어 사용 행위\n*해당 게시물은 시사, 이슈 게시판에만 작성 가능합니다.";
+    const postContentPlaceholder = "내용을 입력해 주세요.\n\n포 올은 누구나 기분 좋게 참여할 수 있는 커뮤니티를 만들기 위해 커뮤니티 이용규칙을 제정하여 운영하고 있습니다. 위반 시 게시물이 삭제되고 서비스 이용이 일정 기간 제한될 수 있습니다.\n\n 아래는 이 게시판에 해당하는 핵심 내용에 대한 요약 사항이며, 게시물 작성 전 커뮤니티 이용규칙 전문을 반드시 확인하시기 바랍니다.\n\n※ 정치, 사회 관련 행위 금지\n-국가기관, 정치 관련 단체, 언론, 시민단체에 대한 언급 혹인 이와 관련한 행위\n-정책, 외교 또는 정책, 정파에 대한 의견, 주장 및 이념, 가치관을 드러내는 행위\n-성별, 종교, 인종, 출신, 지역, 직업, 이념 등 사회적 이유에 대한 언급 혹은 이와 관련한 행위\n,-위와 같은 내용으로 유추될 수 있는 비유, 은어 사용 행위\n*해당 게시물은 시사, 이슈 게시판에만 작성 가능합니다.\n\n※ 홍보 및 판매 관련 행위 금지\n- 영리 여부와 관계 없이 사업체, 기관, 단체, 개인에게 직간접적으로 영향을 줄 수 있는 게시물 작성 행위\n-위와 관련된 것으로 의심되거나 예상될 수 있는 바이럴 홍보 및 명칭, 단어 언급 행위";
     const onChangePostTitle = useCallback((e)=>setPostTitle(e.target.value),[]);
     const onChangePostContent = useCallback((e)=>setPostContent(e.target.value),[]);
 
@@ -88,26 +89,58 @@ const PopupBoardPage = () => {
                             fontSize: '0.625rem', fontStyle: 'normal', fontWeight: '500',
                             lineHeight: 'normal', letterSpacing: '-0.01031rem'
                         }}>새 글을 작성해주세요!</p>
-                        {/*<a onClick={() => setNewPost(true)}*/}
-                        {/*    disabled={!sessionStorage.getItem("user_id")}>새 글을 작성해주세요!</a>*/}
                     </div>
                 </div>
                 <div>
                     {newPost ? (
                         <div>
-                            <input value={postTitle} onChange={onChangePostTitle} placeholder={"글 제목"}/>
-                            <input value={postContent} onChange={onChangePostContent}
-                                   placeholder={postContentPlaceholder}/>
-                            <label>
-                                <input type={"file"}
-                                       accept="image/*"
-                                       onChange={(e) => setPostImage(Array.from(e.target.files))}
-                                       style={{display: "none"}}
-                                       multiple={true}
-                                />
-                                <div>첨부파일</div>
-                            </label>
-                            <button onClick={UploadPost}>글 올리기</button>
+                            <div>
+                                <input value={postTitle} onChange={onChangePostTitle} placeholder={"제목을 입력해주세요"} style={{
+                                    fontSize: '1.25rem', fontStyle: 'normal', fontWeight: '500',
+                                    lineHeight: 'normal', letterSpacing: '-0.01031rem', margin:"0.5rem",
+                                    border:0
+                                }}/>
+                            </div>
+                            <div style={{display:"flex", margin:"0.5rem"}}>
+                                <div style={{width:"1.5rem", height:"1.5rem", borderRadius:"1.5rem", overflow:"hidden"}}>
+                                    <ImageViewer val={null}/>
+                                </div>
+                                <div style={{color:"#0788FF",fontStyle:"0.875rem"}}>{sessionStorage.getItem("user_id")}</div>
+                            </div>
+                            <div>
+                                <textarea value={postContent} onChange={onChangePostContent}
+                                          placeholder={postContentPlaceholder} style={{
+                                    fontSize: '0.875rem', fontStyle: 'normal', fontWeight: '400',
+                                    lineHeight: 'normal', letterSpacing: '-0.01031rem', margin:"0.5rem",
+                                    border:0, width:"100%", height:"25rem"
+                                }}/>
+                            </div>
+                            <div style={{display:"flex",justifyContent:"space-between"}}>
+                                <label>
+                                    <input type={"file"}
+                                           accept="image/*"
+                                           onChange={(e) => setPostImage(Array.from(e.target.files))}
+                                           style={{display: "none"}}
+                                           multiple={true}
+                                    />
+                                    <div>
+                                        <img src={clip} alt="pencilImg" style={{
+                                            width: '0.625rem', height: '0.625rem',
+                                            flexShrink: 0, marginLeft: '1rem'
+                                        }}/>
+                                    </div>
+                                </label>
+                                <div style={{textAlign:"right", paddingRight:"1rem"}}>
+                                    <div onClick={UploadPost}>
+                                        <img src={pencilImg} alt="pencilImg" style={{
+                                            width: '0.625rem', height: '0.625rem',
+                                            flexShrink: 0
+                                        }}/>
+                                    </div>
+                                </div>
+
+                            </div>
+
                             <div>
                                 {postImage ? postImage.map((img, idx) => (
                                     <ImagePreView img={img}/>
