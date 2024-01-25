877f8b19818efa08d81a6872a5bcb49dd0744d52
kimyt0511 <kimyt0511@yonsei.ac.kr>
2024-01-26 05:27:27 +0900

    [01.26 윤태] 오류 수정

--- src/main/frontend/src/utils/TimeUtil.js
+++ src/main/frontend/src/utils/TimeUtil.js
@@ -26,11 +26,11 @@ export const TimeUtil = {
         const from = new Date(json).getTime();
         const now = new Date().getTime();
         const sec = Math.floor((now - from)/1000);
-        if (sec < 10) return "방금전";
-        else if (sec < 60) return sec+"초 전";
-        else if (sec < 60*60) return Math.floor(sec / 60)+"분 전";
-        else if (sec < 24*60*60) return Math.floor(sec / 60 / 60) + "시간 전";
-        else return Math.floor(sec / 60 / 60 / 24) + "일 전";
+        if (sec < 10) return "방금";
+        else if (sec < 60) return sec+"초";
+        else if (sec < 60*60) return Math.floor(sec / 60)+"분";
+        else if (sec < 24*60*60) return Math.floor(sec / 60 / 60) + "시간";
+        else return Math.floor(sec / 60 / 60 / 24) + "일";
     },
     toChatTime: (json)=>{
         const date = new Date(json).toLocaleDateString().replaceAll(".", "/").replaceAll(" ","");
