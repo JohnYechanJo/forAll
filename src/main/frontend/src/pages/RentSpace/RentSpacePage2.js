7717f6f3b41f0d829361c78896f85a6202471b3b
kimyt0511 <kimyt0511@yonsei.ac.kr>
2024-01-26 08:45:11 +0900

    [01.26 윤태] rentSpacepage 1,2 스타일 추가

--- src/main/frontend/src/pages/RentSpace/RentSpacePage2.js
+++ src/main/frontend/src/pages/RentSpace/RentSpacePage2.js
@@ -1,9 +1,13 @@
 import {useLocation, useNavigate} from "react-router-dom";
 import ImageViewer from "../../components/ImageViewer";
 import Calendar from "../../components/Calender";
-import {useCallback, useEffect, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import Alert from "../../components/Alert";
-
+import "../../style/RentSpace.css";
+import arrowleft from "../../components/icons/arrowleft.png";
+import {AddressUtil} from "../../utils/AddressUtil";
+import plusButton from "../../components/icons/plusButton.png";
+import minusButton from "../../components/icons/minusButton.png";
 const RentSpacePage2 = () => {
     const location = useLocation();
     const data = {...location.state};
@@ -36,33 +40,60 @@ const RentSpacePage2 = () => {
         console.log(rentDay);
     }, [rentDay]);
     return (
-        <div>
-            <div>
-                <button onClick={()=>navigate(-1)}>{"<"}</button>
-                <h1>날짜 및 시간</h1>
+        <div className={"rent_space_container"}>
+            <div style={{display:"flex", alignItems:"center"}}>
+                <img src={arrowleft} alt="arrowleft"
+                     onClick={()=>navigate(-1)}
+                     style={{width: '0.7rem', height: '1.1rem', flexShrink: 0, paddingLeft:"1rem"}}/>
+                <div style={{textAlign:"center", flexGrow:"1"}}>
+                    <p style={{fontSize:"1rem", padding:"1rem"}}>날짜 및 시간</p>
+                </div>
+
             </div>
             <div>
                 <ImageViewer val={data.spaceImage}/>
-                <p>{data.spaceAddress} | {data.spaceName}</p>
+                <p style={{fontSize:"1rem", paddingLeft:"1rem"}}>{AddressUtil.extraction(data.spaceAddress)} | {data.spaceName}</p>
             </div>
-            <div>
-                <h1>날짜</h1>
-                <p>대관 희망 날짜</p>
+            <div style={{margin:"1rem"}}>
+                <p style={{fontSize:"1rem"}}>• 날짜<span style={{ color: '#FF2929' }} >*</span></p>
+                <hr style={{ height: "2px", backgroundColor: "black" }} />
+                <p>대관 희망 날짜<span style={{color:"red"}}>*</span></p>
+            </div>
+            <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
                 <Calendar setDate={setRentDay}/>
-                <p>트라이얼 희망 날짜</p>
+            </div>
+            <p style={{margin:"1rem"}}>트라이얼 희망 날짜<span style={{color:"red"}}>*</span></p>
+            <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
                 {data.ableTrial ? ( <Calendar setDate={setTrialDay}/>) : (<p>오너가 트라이얼이 불가하다고 지정한 매장입니다.</p>)}
-                <p>셰프 수</p>
-                <div>
-                    <button onClick={substractChef} disabled={chefNum <= 1}>{"-"}</button>
-                    <p>{chefNum}</p>
-                    <button onClick={addChef} disabled={chefNum >= data.capacity}>{"+"}</button>
-                    <p>이 주방의 최대 수용 셰프 수는 {data.capacity}명입니다.</p>
+            </div>
+            <p style={{margin:"1rem"}}>셰프 수<span style={{color:"red"}}>*</span></p>
+            <div style={{display:"flex", paddingLeft:"1rem"}}>
+                <div style={{margin:"0 0.5rem 0 0.5rem"}} onClick={()=>{
+                    if (chefNum <= 1) return;
+                    substractChef();
+                }}>
+                    <img src={minusButton} alt="minusButton"
+                         style={{width: '1.125rem', height: '1.125rem', flexShrink: 0, display:"block"}}/>
                 </div>
+                <p>{chefNum}명</p>
+                <div style={{margin:"0 0.5rem 0 0.5rem"}} onClick={()=>{
+                    if (chefNum >= data.capacity) return;
+                    addChef();
+                }}><img src={plusButton} alt="plusButton"
+                        style={{width: '1.125rem', height: '1.125rem', flexShrink: 0, display:"block"}}/></div>
+
             </div>
-            <div>
-                <button onClick={()=>navigate(-1)}>이전</button>
-                <button onClick={submit}>다음</button>
+            <p style={{margin:"1rem"}}>이 주방의 최대 수용 셰프 수는 {data.capacity}명입니다.</p>
+            <div style={{display:"flex"}}>
+                <button style={{ marginLeft: 'auto', backgroundColor: "#FF4F4F", width: '50%', bottom: '0', height: '3.125rem', color: 'white', border: 'none', lineHeight: '1.875rem', textAlign: 'center' }}
+                        onClick={() => navigate(-1)}
+                >
+                    이전</button>
+                <button style={{ marginLeft: 'auto', backgroundColor: "#525252", width: '50%', bottom: '0', height: '3.125rem', color: 'white', border: 'none', lineHeight: '1.875rem', textAlign: 'center' }}
+                        onClick={submit}
+                >다음</button>
             </div>
+
             <Alert content={alertContent} isOpen={isAlertOpen} setIsOpen={setIsAlertOpen} />
         </div>
     )
