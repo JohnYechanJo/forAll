3451e5b669308419d753c2a7546ee70457477050
kimyt0511 <kimyt0511@yonsei.ac.kr>
2024-01-26 08:45:11 +0900

    [01.26 윤태] rentSpacepage 1,2 스타일 추가

--- src/main/frontend/src/pages/RentSpace/RentSpacePage.js
+++ src/main/frontend/src/pages/RentSpace/RentSpacePage.js
@@ -1,14 +1,13 @@
-import {useState, useCallback, useEffect} from "react";
+import React, {useState, useCallback, useEffect} from "react";
 import {useNavigate, useParams} from 'react-router-dom';
 import axios from "axios";
 import ImageSlider from "../../components/ImageSlider";
-
-
 import {ChatRoomCategory, KitchenFeat} from "../../utils/enums";
-
-
 import useDidMountEffect from "../../utils/hooks/useDidMountEffect";
 import {AddressUtil} from "../../utils/AddressUtil";
+import "../../style/RentSpace.css";
+import arrowright from "../../components/icons/arrowright.png";
+import hearImg from "../../components/icons/heart.png";
 const RentSpacePage = () => {
     const params = useParams();
     const navigate = useNavigate();
@@ -20,10 +19,6 @@ const RentSpacePage = () => {
         axios.get("/api/v1/space/"+params.id)
             .then((res) => {
                 setData(res.data);
-
-
-                // console.log(res.data);
-
             })
             .catch((err) => console.error(err));
     }, []);
@@ -31,8 +26,6 @@ const RentSpacePage = () => {
         setImages1([data.mainImage, ...data.hallImage]);
         setEquipments(data.equip ? data.equip.split(",") : []);
     }, [data]);
-
-
 
     const submit = () => {
         // Todo 셰프 등록이 되었는지 확인
@@ -51,50 +44,57 @@ const RentSpacePage = () => {
 
 
     return(
-        <div>
+        <div className={"rent_space_container"}>
             <div>
                 <ImageSlider images={images1}/>
             </div>
-            <div>
-                <h1>{AddressUtil.extraction(data.address)} | {data.name}</h1>
-                <p>{data.priceSet}원 | {data.ableDate}</p>
+            <div style={{marginBottom:"2rem"}}>
+                <div className={"space_title"} style={{paddingLeft:"1rem"}}>{AddressUtil.extraction(data.address)} | {data.name}</div>
+                <p style={{margin:"1rem"}}>{data.priceSet}원 | {data.ableDate}</p>
                 {/*Todo : 관리자 채팅 연결*/}
-                <p>*영업일 대관 시 800,000원 | 별도 문의 바람</p>
-
-
-                <p onClick={()=>navigate("/profile/"+data.userId)}>프로필 보기</p>
-                <p onClick={()=> {
-                    if (!sessionStorage.getItem("user_id")) return;
-                    navigate("/chatRoom", {
-                        state: {
-                            partner: data.userId,
-                            category: ChatRoomCategory.Reservation
-                        }
-                    })
-                }}>채팅 보내기</p>
-
+                <div style={{display:"flex", justifyContent:"space-between"}}>
+                    <p style={{marginLeft:"1rem"}}>*영업일 대관 시 800,000원 | 별도 문의 바람</p>
+                    <div style={{textAlign:"right", display:"flex"}}>
+                        <p className={"underline"} onClick={()=>navigate("/profile/"+data.userId)}>프로필 보기</p>
+                        <p onClick={()=> {
+                            if (!sessionStorage.getItem("user_id")) return;
+                            navigate("/chatRoom", {
+                                state: {
+                                    partner: data.userId,
+                                    category: ChatRoomCategory.Reservation
+                                }
+                            })
+                        }} style={{padding:"0 0.5rem 0 0.5rem",textDecorationLine: "underline"}}>채팅 보내기</p>
+                    </div>
+                </div>
             </div>
             <div>
-                <h1>공간 소개</h1>
-                <p>{data.spaceBrief}</p>
-                <p onClick={() => navigate("/rentSpaceInfo1/"+params.id)}>더 보기</p>
+                <p style={{fontSize:"1rem", margin:"1rem 0 1rem 0"}}>• 공간 소개</p>
+                <div>
+                    <p style={{paddingLeft:"1rem"}}>공간명<span style={{color:"red"}}>*</span></p>
+                    <div className={"data_input"}><p style={{paddingLeft:"0.6rem"}}>{data.name}</p></div>
+                </div>
+                <div style={{marginTop:"1rem"}}></div>
+                <div>
+                    <p style={{paddingLeft:"1rem"}}>공간 한 줄 소개<span style={{color:"red"}}>*</span></p>
+                    <div className={"data_input"}><p style={{paddingLeft:"0.6rem"}}>{data.spaceBrief}</p></div>
+                </div>
+                <div style={{marginTop:"1rem"}}></div>
+                <p className={"more_info"} onClick={() => navigate("/rentSpaceInfo1/"+params.id)} style={{paddingLeft:"1rem"}}>더 보기</p>
             </div>
             <div>
-                <h1>공간 정보</h1>
-                <p>주소(위치)</p>
-                <input
-                    value={data.address}
-                    disabled={true}
-                />
-                <input
-                    value={data.addressBrief}
-                    disabled={true}
-                />
-                <p onClick={() => navigate("/rentSpaceInfo2/"+params.id)}>더 보기</p>
+                <p style={{fontSize:"1rem", margin:"1rem 0 1rem 0"}}>• 공간 정보</p>
+                <div>
+                    <p style={{paddingLeft:"1rem"}}>주소(위치)<span style={{color:"red"}}>*</span></p>
+                    <div className={"data_input"}><p style={{paddingLeft:"0.6rem"}}>{data.address}</p></div>
+                    <div style={{marginTop:"0.5rem"}}></div>
+                    <div className={"data_input"}><p style={{paddingLeft:"0.6rem"}}>{data.addressBrief}</p></div>
+                </div>
+                <p onClick={() => navigate("/rentSpaceInfo2/"+params.id)} className={"more_info"} style={{paddingLeft:"1rem"}}>더 보기</p>
             </div>
             <div>
-                <h1>주방정보</h1>
-                <p>주방 특성</p>
+                <p style={{fontSize:"1rem", margin:"1rem 0 1rem 0"}}>• 주방 정보</p>
+                <p style={{paddingLeft:"1rem"}}>주방 특성<span style={{color:"red"}}>*</span></p>
                 <div style={{display: "flex"}}>
                     <button className="button"
                             name="kitchen"
@@ -136,45 +136,64 @@ const RentSpacePage = () => {
                         폐쇄형
                     </button>
                 </div>
-                <p>오픈형이 무엇인가요?</p>
-                <p>대면형이 무엇인가요?</p>
-                <p>폐쇠형이 무엇인가요?</p>
-                <div>
-                    <p>화구</p>
-                    <input
-                        value={data.fireholeNum}
-                        disabled={true}
-                    />
+                <div style={{display: "flex", marginBottom:"1rem"}}>
+                    <p style={{width: "100px",
+                        flex: "1",
+                        marginLeft: "10px"}}>오픈형이 무엇인가요?</p>
+                    <p style={{width: "100px",
+                        flex: "1",
+                        marginLeft: "10px"}}>대면형이 무엇인가요?</p>
+                    <p style={{width: "100px",
+                        flex: "1",
+                        marginLeft: "10px"}}>폐쇠형이 무엇인가요?</p>
+                </div>
+                <div style={{marginBottom:"1rem"}}>
+                    <p style={{paddingLeft:"1rem"}}>화구<span style={{color:"red"}}>*</span></p>
+                    <div className={"data_input"}><p style={{paddingLeft:"0.6rem"}}>{data.fireholeNum + "구"}</p></div>
                 </div>
                 <div>
-                    <h4>확보된 주방기계</h4>
-                    <div style={{display:"flex", flexDirection:"row",justifyContent:"space-evenly"}}>
+                    <p style={{paddingLeft:"1rem"}}>확보된 주방기계<span style={{color:"red"}}>*</span></p>
+                    <div style={{display:"flex", flexDirection:"row",justifyContent:"space-evenly", marginTop:"1rem"}}>
                         <div className={equipments.includes("튀김기") === true ? "btn_selected" : ""}>튀김기</div>
                         <div className={equipments.includes("오븐") === true ? "btn_selected" : ""}>오븐</div>
                         <div className={equipments.includes("식기세척기") === true ? "btn_selected" : ""}>식기세척기</div>
                         <div className={equipments.includes("제빙기") === true ? "btn_selected" : ""}>제빙기</div>
                     </div>
                 </div>
-                <div>
-                    <h4>추가 사용 가능 기계</h4>
+                <div style={{marginTop:"1rem"}}>
+                    <p style={{paddingLeft:"1rem"}}>추가 사용 가능 기계<span style={{color:"red"}}>*</span></p>
                     <div
                         style={{
                             border: "2px solid gray",
                             borderRadius: "2px",
-                            width: "100%",
+                            width: "21.875rem",
                             height: "10vh",
+                            margin: "0 1rem 0 1rem",
+                            fontSize:"0.625rem",
+                            padding:"0.5rem 0 0 0.5rem"
                         }}
                     >
                         {data.equipExtra}
                     </div>
                 </div>
-                <p onClick={() => navigate("/rentSpaceInfo3/"+params.id)}>더 보기</p>
+                <p onClick={() => navigate("/rentSpaceInfo3/"+params.id)} className={"more_info"} style={{paddingLeft:"1rem"}}>더 보기</p>
                 <div>
-                    <div>고객센터</div>
-                    <button>찜하기</button>
-
-
-                    <button onClick={submit}>예약하기</button>
+                    <div style={{display:"flex", margin:"1rem", justifyContent:"space-between"}}>
+                        <p style={{fontSize:"1rem"}}>고객센터</p>
+                        <div style={{textAlign:'right'}}>
+                            <img src={arrowright} alt="arrowright"
+                                 style={{width: '0.7rem', height: '1.1rem', flexShrink: 0}}/>
+                        </div>
+                    </div>
+                    <div style={{display:"flex", margin:"1rem", justifyContent:"space-between"}}>
+                        <div>
+                            <img src={hearImg} alt="heartImg"
+                                 style={{width: '1.9rem', height: '1.9rem', flexShrink: 0}}/>
+                        </div>
+                        <div className={"submit_button"} style={{textAlign:"right"}} onClick={submit}>
+                            <p>예약하기</p>
+                        </div>
+                    </div>
 
 
                 </div>
--- src/main/frontend/src/utils/regularExpressions 2.js
+++
@@ -1,2 +0,0 @@
-export const phoneNum =  /^[0-9]{11}$/;
-export const email = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
