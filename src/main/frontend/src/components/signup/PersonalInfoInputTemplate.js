43cc6fbd1da2af82ccddd5b169f03835fe0fc301
johnjo <johnjo1116@gmail.com>
2024-01-27 20:10:09 +0900

    [01.27 예찬] signUp, personalInfoModify page box size 조절 완료+비밀번호 숨기기 구현 완료

--- src/main/frontend/src/components/signup/PersonalInfoInputTemplate.js
+++ src/main/frontend/src/components/signup/PersonalInfoInputTemplate.js
@@ -1,4 +1,4 @@
-import {useCallback, useEffect} from "react";
+import {useCallback, useEffect, useState} from "react";
 import { Gender } from "../../utils/enums";
 import axios from "axios";
 import "../../components/Styles.css";
