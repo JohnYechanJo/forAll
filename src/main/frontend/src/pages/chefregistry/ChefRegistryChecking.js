b6a0c8fe8327b72f88e2633f6e33cf7696a30d07
Keunil Song <keunilsong1021@gmail.com>
2024-01-29 22:47:56 +0900

    [01. 29 근일]chefregistry&chefregistrychecking page 수정

--- src/main/frontend/src/pages/chefregistry/ChefRegistryChecking.js
+++ src/main/frontend/src/pages/chefregistry/ChefRegistryChecking.js
@@ -1,9 +1,9 @@
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import checkIcon from "../../components/icons/check.png";
 import "../../components/Styles.css";
 import Alert from "../../components/Alert";
 const ChefRegistryCheckingPage = () => {
-    
+
     const navigate = useNavigate();
     const id = sessionStorage.getItem("user_id");
     const name = sessionStorage.getItem("name");
@@ -14,25 +14,26 @@ const ChefRegistryCheckingPage = () => {
             flexDirection: "column",
             justifyContent: "center",
             alignItems: "center",
-            height: "100vh"
+            height: "100vh",
+            
         }} >
-            <img src={checkIcon} alt="check" style={{width:"3.33rem", height:"3.33rem",flexShrink:"0"}} />
-            <p style={{fontSize:"1.25rem", fontWeight:"700",marginBottom:"7.5rem"}} >{name}님 감사합니다!</p>
-            <div style={{fontSize:"0.938rem", fontWeight:"400",gap:"7.5rem", alignItems:"center", textAlign:"center"}} >
+            <img src={checkIcon} alt="check" style={{ width: "3.33rem", height: "3.33rem", flexShrink: "0",marginTop:'7.5rem' }} />
+            <p style={{ fontSize: "1.25rem", fontWeight: "700" }} >{name}님 감사합니다!</p>
+            <div style={{ fontSize: "0.938rem", fontWeight: "400", gap: "7.5rem", alignItems: "center", textAlign: "center",marginBottom:'7.5rem' }} >
                 <p>포올에서 꼼꼼히 검수 후</p>
                 <p>셰프님의 등록 결과를 알려드릴게요.</p>
             </div>
-            <div style={{display:'flex',flexDirection:'column',justifyContent:'flex-start'}} >
-            <div>
-                <span style={{fontSize:"0.938rem", fontWeight:"700"}} >아이디</span>
-                <span style={{fontSize:"0.938rem", fontWeight:"400",marginLeft:'1rem'}} >{id}</span>
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', }} >
+                <div>
+                    <span style={{ fontSize: "0.938rem", fontWeight: "700" }} >아이디</span>
+                    <span style={{ fontSize: "0.938rem", fontWeight: "400", marginLeft: '1rem' }} >{id}</span>
+                </div>
+                <div style={{ marginBottom: "7.5rem" }} >
+                    <span style={{ fontSize: "0.938rem", fontWeight: "700" }} >이메일</span>
+                    <span style={{ fontSize: "0.938rem", fontWeight: "400", marginLeft: '1rem' }} >{email}</span>
+                </div>
             </div>
-            <div style={{marginBottom:"7.5rem"}} >
-                <span style={{fontSize:"0.938rem", fontWeight:"700"}} >이메일</span>
-                <span style={{fontSize:"0.938rem", fontWeight:"400",marginLeft:'1rem'}} >{email}</span>
-            </div>
-            </div>
-            <button onClick={()=>{navigate("/");}} className="bottom_button" style={{backgroundColor:"black",position:"fixed"}} >시작하기</button>
+            <button onClick={() => { navigate("/"); }} className="bottom_button" style={{ backgroundColor: "black", position: "fixed" }} >시작하기</button>
 
         </div>
     )
