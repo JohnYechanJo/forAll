c82193398f26a5d738efd220de146b899c34231d
Keunil Song <keunilsong1021@gmail.com>
2024-01-30 07:14:45 +0900

    [01. 30 근일]admin page 스타일

--- src/main/frontend/src/pages/admin/AdminMainPage.js
+++ src/main/frontend/src/pages/admin/AdminMainPage.js
@@ -35,56 +35,58 @@ const AdminMainPage = () => {
     return(
         <div style={{display:"flex"}}>
             <div className={"sidebar"}>
-                <h1>포 올 관리자 페이지</h1>
+                <a className="font-weight-bold" style={{}} >포 올 관리자 페이지</a>
 
                 <div>
-                    <h1>공간 등록</h1>
+                    <a className="font-normal" >공간 등록</a>
                     <p onClick={()=>setStates("space", SpaceState.PENDING)}>대기중</p>
                     <p onClick={()=>setStates("space", SpaceState.APPROVE)}>승인한 매장</p>
                     <p onClick={()=>setStates("space", SpaceState.REJECT)}>거절한 매장</p>
                 </div>
                 <div>
-                    <h1>셰프 등록</h1>
+                    <a className="font-normal" >셰프 등록</a>
                     <p onClick={()=>setStates("chef", ChefState.PENDING)}>대기중</p>
                     <p onClick={()=>setStates("chef", ChefState.APPROVE)}>승인한 셰프</p>
                     <p onClick={()=>setStates("chef", ChefState.REJECT)}>거절한 셰프</p>
                 </div>
                 <div>
-                    <h1>예약 확정</h1>
+                    <a className="font-normal" >예약 확정</a>
                     <p onClick={()=>setStates("reservation", ReservationState.PENDING)}>대기중</p>
                     <p onClick={()=>setStates("reservation", ReservationState.APPROVE)}>승인한 예약</p>
                     <p onClick={()=>setStates("reservation", ReservationState.REJECT)}>거절한 예약</p>
                 </div>
                 <div>
-                    <h1>예약 취소</h1>
+                    <a className="font-normal" >예약 취소</a>
                     <p onClick={()=>setStates("reservation", ReservationState.CANCELPENDING)}>대기중</p>
                     <p onClick={()=>setStates("reservation", ReservationState.CANCELAPPROVE)}>승인한 예약취소</p>
                     <p onClick={()=>setStates("reservation", ReservationState.CANCELREJECT)}>거절한 예약취소</p>
                 </div>
                 <div>
-                    <h1>고객 센터</h1>
+                    <a className="font-normal" >고객 센터</a>
                 </div>
             </div>
             <div className={"container"}>
-                <h1>{tableName}</h1>
-                <div className={"data_table"}>
+                <p style={{fontSize:'1.875rem',fontWeight:'600'}} >• {tableName}</p>
+                <div className={"table_header"}>
                     <div className={"table_header"}>
-                        <p>이름</p>
+                        <p style={{marginLeft:'2rem'}} >이름</p>
                         <p>상세 정보</p>
-                        <p>승인/거절</p>
+                        <p style={{marginRight:'4.5rem'}} >승인/거절</p>
                     </div>
                 </div>
                 {dataList ? (
                     dataList.map((data, idx) => (
                         <div key={idx} className={"row"}>
-                            <p>{data.name}</p>
+                            <p style={{marginLeft:'2rem'}} >{data.name}</p>
                             <p onClick={()=>navigate("/admin"+category+"ViewPage1", {state: {
                                     ...data,
                                     category: category,
                                     state: state
-                                }})}>더보기</p>
+                                }})}
+                                style={{color:'table_gray',textDecorationLine:'underline'}}
+                                >더보기</p>
                             {state === "Pending" ? (
-                                <div className={"button-set"}>
+                                <div className={"button-set"} style={{marginRight:'2rem'}} >
                                     <div className={"button-approve"} onClick={()=>confirmObject(data.id, "Approve")}>
                                         <p>승인</p>
                                     </div>
@@ -92,7 +94,14 @@ const AdminMainPage = () => {
                                         <p>거절</p>
                                     </div>
                                 </div>
-                            ) :null}
+                            ) : tableName === "승인" ? (
+                                <div className="button-approve" style={{marginRight:'4rem'}} >
+                                    <p>승인</p>
+                                </div>) : (<div className="button-reject" style={{marginRight:'4rem'}} >
+                                <p>거절</p>
+                            </div>)
+                            
+                            }
                         </div>
                     ))
                 ) : null}
